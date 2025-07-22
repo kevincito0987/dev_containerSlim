6a0dadb2e8cedf67159563246f4693e263e41e81
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-22 13:26:28 +0000

    perf: :zap: For now is functional.

--- index.php
+++ index.php
@@ -19,7 +19,19 @@ $app->get('/', function(Request $req, Response $res, array $args) {
 
 //Get/camper
 //Post/camper
-//Put/camper
-//Delete/camper
+//Put/camper/1
+//Patch/camper/1
+//Delete/camper/1
+
+$app->get('/camper', function(Request $req, Response $res, array $args) {
+
+    $params = $req->getQueryParams();
+    $name = $params['name'] ?? "default";
+    $skill = $params['skill'] ?? "default";
+    $res->getBody()->write(
+        json_encode([$name, $skill])
+    );
+        return $res;
+});
 
 $app->run();

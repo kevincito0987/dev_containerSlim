0495cfe8cf61ebe9b68a548f1aca5f5ce06f8039
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-24 23:44:40 +0000

    perf: :zap: I've done thsi use case.

--- src/UseCases/DeleteCamper.php
+++ src/UseCases/DeleteCamper.php
@@ -1,27 +1,18 @@
 <?php
 
 namespace App\UseCases;
+use App\Domain\Repositories\CamperRepositoryInterface;
 
 
 
 
-class DeleteCamper {}
+class DeleteCamper {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public function __construct(private CamperRepositoryInterface $repo) {}
+    
+    public function execute(int $documento): bool {
+        return $this->repo->delete($documento);
+    }
+}
 
 ?>

c7986f739a8ec17536000635f07ff690a2a18653
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-25 00:30:04 +0000

    perf: :zap: I've improved this get all function, tomorrow i'll continue with the others methods.

--- src/UseCases/GetAllCampers.php
+++ src/UseCases/GetAllCampers.php
@@ -9,6 +9,36 @@ class GetAllCampers
     public function __construct(private CamperRepositoryInterface $repo) {}
 
     public function execute(): array {
-        return $this->repo->getAll();
+        $campers = $this->repo->getAll();
+        return array_map([$this, 'transform'], $campers);
+    }
+
+    private function transform(array $camper): array {
+        unset($camper['created_at'], $camper['updated_at']);
+    
+        $camper['nivel_ingles'] = $this->nivelTexto($camper['nivel_ingles']);
+        $camper['nivel_programacion'] = $this->nivelTexto($camper['nivel_programacion']);
+    
+        $camperOrdenado = [
+            'ID' => $camper['id'],
+            'nombre' => $camper['nombre'],
+            'edad' => $camper['edad'],
+            'documento' => $camper['documento'],
+            'tipo_documento' => $camper['tipo_documento'],
+            'nivel_ingles' => $camper['nivel_ingles'],
+            'nivel_programacion' => $camper['nivel_programacion'],
+        ];
+    
+        return $camperOrdenado;
+    }
+    
+
+    private function nivelTexto(int $nivel): string {
+        return match(true) {
+            $nivel <= 2 => 'BAJO',
+            $nivel > 2 &&  $nivel <= 4 => 'MEDIO',
+            $nivel > 4 &&  $nivel => 'ALTO',
+            default => 'desconocido',
+        };
     }
 }

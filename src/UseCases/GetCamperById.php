7b4427bc71dbc30c9fcd91c05c1fd0de4897ff94
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-25 23:17:59 +0000

    perf: :zap: I've updated this method achieving with the challenge.

--- src/UseCases/GetCamperById.php
+++ src/UseCases/GetCamperById.php
@@ -7,11 +7,37 @@ use App\Domain\Repositories\CamperRepositoryInterface;
 
 class GetCamperById
 {
-
     public function __construct(private CamperRepositoryInterface $repo) {}
 
-    public function execute(int $documento): ?Camper
+    public function execute(int $documento): ?array
     {
-        return $this->repo->getById($documento);
+        $camper = $this->repo->getById($documento);
+
+        if (!$camper) return null;
+
+        unset($camper['created_at'], $camper['updated_at']);
+
+        $camper['nivel_ingles'] = $this->nivelTexto($camper['nivel_ingles']);
+        $camper['nivel_programacion'] = $this->nivelTexto($camper['nivel_programacion']);
+
+        return [
+            'ID' => $camper['id'],
+            'nombre' => $camper['nombre'],
+            'edad' => $camper['edad'],
+            'documento' => $camper['documento'],
+            'tipo_documento' => $camper['tipo_documento'],
+            'nivel_ingles' => $camper['nivel_ingles'],
+            'nivel_programacion' => $camper['nivel_programacion'],
+        ];
+    }
+
+    private function nivelTexto(int $nivel): string {
+        return match(true) {
+            $nivel <= 2 => 'BAJO',
+            $nivel > 2 &&  $nivel <= 4 => 'MEDIO',
+            $nivel > 4 &&  $nivel => 'ALTO',
+            default => 'desconocido',
+        };
     }
 }
+

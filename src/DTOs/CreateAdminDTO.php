68b6ba32504361202138434b31654734b5ff762c
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-26 22:31:58 +0000

    perf: :zap: Ive updated this dto to work it in the use case.

--- src/DTOs/CreateAdminDTO.php
+++ src/DTOs/CreateAdminDTO.php
@@ -3,7 +3,16 @@
 namespace App\DTOs;
 
 
-class CreateAdminDTO {}
+class CreateAdminDTO {
+    public string $nombre;
+    public string $email;
+    public string $password;
 
+    public function __construct(string $nombre, string $email, string $password) {
+        $this->nombre = $nombre;
+        $this->email = $email;
+        $this->password = $password;
+    }
+}
 
 ?>

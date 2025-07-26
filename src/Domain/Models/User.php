aa9bc399eb7c95c00401f790633a0390cd99d6ed
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-26 17:44:52 +0000

    perf: :zap: I've updated this model.

--- src/Domain/Models/User.php
+++ src/Domain/Models/User.php
@@ -1,10 +1,19 @@
 <?php 
 
 namespace App\Domain\Models;
+use Illuminate\Database\Eloquent\Model;
 
 
 
-class User {}
+class User extends Model  {
+
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id'; // Llave primaria
+    public $timestamps = false; // Habilita el uso de created_at y updated_at
+    protected $fillable = ['nombre', 'email', 'password', 'rol']; // Columnas habilitadas para la insercion
+    protected $hidden = ['password'];
+
+}
 
 
 

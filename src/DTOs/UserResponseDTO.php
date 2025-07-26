0cac5c6d930ed0c4e5baa9854e5274e526c1e94a
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-26 22:04:53 +0000

    perf: :sparkles: I've created our user response dto to work it.

---
+++ src/DTOs/UserResponseDTO.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App\DTOs;
+
+
+class UserResponseDTO {}
+
+
+
+
+
+
+?>
--- src/UseCases/CreateUser.php
+++ src/UseCases/CreateUser.php
@@ -1,11 +1,37 @@
 <?php 
 
 namespace App\UseCases;
+use App\Domain\Repositories\UserRepositoryInterface as UserRepository;
+use App\DTOs\CreateUserDTO;
 
 
 class CreateUser {
+    private UserRepository $repository;
 
+    public function __construct(UserRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    public function execute(CreateUserDTO $dto): UserResponseDTO {
+        $data = [
+            'nombre' => $dto->nombre,
+            'email' => $dto->email,
+            'rol' => $dto->rol,
+            'password' => password_hash($dto->password, PASSWORD_DEFAULT),
+        ];
+    
+        $userModel = $this->repository->create($data);
+    
+        return new UserResponseDTO(
+            $userModel->id,
+            $userModel->nombre,
+            $userModel->email,
+            $userModel->rol
+        );
+    }
+    
 }
+
 
 
 

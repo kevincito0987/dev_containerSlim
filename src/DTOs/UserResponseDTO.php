9dac0a06cdd2fe3e23c651edc4bc88aaa35a4291
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-26 22:05:20 +0000

    perf: :zap: I've updated this dto to using in the controller.

--- src/DTOs/UserResponseDTO.php
+++ src/DTOs/UserResponseDTO.php
@@ -3,11 +3,19 @@
 namespace App\DTOs;
 
 
-class UserResponseDTO {}
+class UserResponseDTO {
+    public int $id;
+    public string $nombre;
+    public string $email;
+    public string $rol;
 
-
-
-
+    public function __construct(int $id, string $nombre, string $email, string $rol) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->email = $email;
+        $this->rol = $rol;
+    }
+}
 
 
 ?>

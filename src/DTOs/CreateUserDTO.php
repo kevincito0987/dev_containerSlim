a1cf9b27c46cbf72f1849a67c1dd8dc422db78e0
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-26 21:51:01 +0000

    perf: :zap: I've updated this dto to using in the controller.

--- src/DTOs/CreateUserDTO.php
+++ src/DTOs/CreateUserDTO.php
@@ -3,7 +3,19 @@
 namespace App\DTOs;
 
 
-class CreateUserDTO {}
+class CreateUserDTO {
+    public string $nombre;
+    public string $email;
+    public string $password;
+    public string $rol;
+
+    public function __construct(string $nombre, string $email, string $password, string $rol = 'user') {
+        $this->nombre = $nombre;
+        $this->email = $email;
+        $this->password = $password;
+        $this->rol = $rol;
+    }
+}
 
 
 ?>

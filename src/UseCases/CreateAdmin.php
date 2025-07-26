b42587476beca39e967e1137cbcd2779facb5220
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-26 22:33:59 +0000

    perf: :zap: Ive updated this usecase to work it in the use controller.

--- src/UseCases/CreateAdmin.php
+++ src/UseCases/CreateAdmin.php
@@ -1,10 +1,35 @@
 <?php
 
 namespace App\UseCases;
+use App\DTOs\CreateAdminDTO;
+use App\DTOs\UserResponseDTO;
+use App\Domain\Repositories\UserRepositoryInterface;
 
 
 class CreateAdmin {
+    private UserRepositoryInterface $repository;
 
+    public function __construct(UserRepositoryInterface $repository) {
+        $this->repository = $repository;
+    }
+
+    public function execute(CreateAdminDTO $dto): UserResponseDTO {
+        $data = [
+            'nombre' => $dto->nombre,
+            'email' => $dto->email,
+            'rol' => 'admin',
+            'password' => password_hash($dto->password, PASSWORD_DEFAULT),
+        ];
+
+        $user = $this->repository->create($data);
+
+        return new UserResponseDTO(
+            $user->id,
+            $user->nombre,
+            $user->email,
+            $user->rol
+        );
+    }
 }
 
 

fa0cb251a97a66c4bf33a7cc390b96a84ef2622e
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-26 17:57:55 +0000

    perf: :zap: I've updated this user eloquent repository.

--- src/Infrastructure/Repositories/EloquentUserRepository.php
+++ src/Infrastructure/Repositories/EloquentUserRepository.php
@@ -1,17 +1,23 @@
 <?php 
 
 namespace App\Infrastructure\Repositories;
+use App\Domain\Models\User;
+use App\Domain\Repositories\UserRepositoryInterface;
+use Exception;
 
 
-class EloquentUserRepository {
+class EloquentUserRepository implements UserRepositoryInterface {
+    public function create(array $data): User
+    {
+        $exists = User::where('email', $data['email'])->first();
+        if ($exists) {
+            //Mostrar un error
+            throw new Exception('Error el usuario ya existe');
+        }
 
+        return User::create($data);
+    }
 }
-
-
-
-
-
-
 
 
 ?>

0c2d77afccaef403ac39c6bc9a1c840477297367
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-26 17:47:51 +0000

    perf: :zap: I've updated this interface.

--- src/Domain/Repositories/UserRepositoryInterface.php
+++ src/Domain/Repositories/UserRepositoryInterface.php
@@ -1,17 +1,14 @@
 <?php 
 
 namespace App\Domain\Repositories;
+use App\Domain\Models\User;
 
 
+class UserRepositoryInterface {
 
+    public function create(array $data): User;
 
-class UserRepositoryInterface {}
-
-
-
-
-
-
+}
 
 
 ?>

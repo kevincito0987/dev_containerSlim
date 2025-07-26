4d5d9150cd43bd2a4f4c947ab770ceb3b7e6b54a
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-07-26 18:17:46 +0000

    perf: :zap: I've updated this camper route.

--- routes/campers.php
+++ routes/campers.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Controllers\CamperController;
+use App\Middleware\AutMiddleware;
+use App\Middleware\RoleMiddleware;
 use Slim\App;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (App $app) {
     $app->group('/campers', function ($group) {
@@ -10,5 +15,16 @@ return function (App $app) {
         $group->post('', [CamperController::class, 'store']);
         $group->put('/{documento}', [CamperController::class, 'update']);
         $group->delete('/{documento}', [CamperController::class, 'destroy']);
-    });
+    })->add(new RoleMiddleware('user'))
+    ->add(new AutMiddleware());
+
+    $app->group('/dashboard', function ($group) {
+        $group->get('', function (Request $request, Response $response, array $args) {
+            $response->getBody()->write("Hello desde admin");
+            return $response;
+        });
+    })->add(new RoleMiddleware('admin'))
+    ->add(new AutMiddleware());
 };
+
+?>
